48e93f38c048b1fdd477665e02fbb774801314a7
Jared Schreiber <jaredkschreiber@gmail.com>
2017-02-22 11:04:24 -1000

    this is another update

--- knn.py
+++ knn.py
@@ -1,20 +1,53 @@
 "K-Nearest-Neighbors Classifier with test data and misc. other algorithms for comparison"
 import csv
 import math
+import statistics
 #functions
 #main function
 def main():
     "main function"
-    loadcsv("car_data_trainset.csv")
+    trainingset = loadcsv("car_data_trainset.csv")
+    testset = loadcsv("car_data_testset.csv")
+    print(testset, trainingset)
     return
-#load CSV file and return list of vectors
+#load a CSV file and return list of vectors
 def loadcsv(fname):
     "loads a csv file"
     rlist = []
-    with open(fname, 'rb') as fvar:
+    with open(fname, 'r') as fvar:
         reader = csv.reader(fvar)
         for row in reader:
             rlist.insert(0, row)
     return rlist
+#Euclidean Distance Function 2 vectors
+def edist2(vector1, vector2):
+    "Euclidean Distance Function for 2 Vectors"
+    #assumes both vectors same dimensionality
+    dist = 0
+    length = len(vector1)
+    for i in range(length):
+        dist += pow((vector1[i] - vector2[i]), length)
+    return math.sqrt(dist)
+#Dot Product
+def dprod(vector1, vector2):
+    "Dot Product"
+    if len(vector1) != len(vector2):
+        return 0
+    return sum(i[0] * i[1] for i in zip(vector1, vector2))
+#Euclidean Distance Function 1 vector
+def edist(vector1):
+    "Euclidean Distance Function for 1 Vector"
+    dist = 0
+    for item in vector1:
+        dist += pow(item, 2)
+    return math.sqrt(dist)
+#Cosine Similarity Function
+def cosim(vector1, vector2):
+    "Cosine Similarity Function"
+    return dprod(vector1, vector2) / edist(vector1) * edist(vector2)
+#Covariance Function
+def cov(vector1, vector2):
+    "Covariance Function"
+    return
 #main block
 main()
